f3a76d3a77c853f4ce3ad9dba8c9c65f2c3fb2ce
prabhkirank12 <prabhkirank2@gmail.com>
2021-04-19 20:41:57 -0400

    Two Sums with two solutions

---
+++ twoSums.js
@@ -0,0 +1,22 @@
+// Complexity is O(n^2) because it has to iterate over the array twice
+var twoSum = function(nums, target) {
+    const len = nums.length;
+    for (let i = 0; i < len; i++){
+        for (let j = i + 1; j < len; j++){
+            if (nums[i] + nums[j] === target) return [i,j];
+        }
+    }    
+};
+
+// A better solution with complexity of O(n)
+function twoSum(nums, target){
+    // first creating a new map called comp which will store complements
+    const comp = new Map();
+    const len = nums.length;
+    // iterate over all the elements, if the complement already exist in the map then it will return the pair
+    // otherwise it will add it
+    for (let i = 0; i < len; i++){
+        if (comp[nums[i]] >= 0) return [comp[nums[i]], i];
+        comp[target-nums[i]] = i
+    }
+}
